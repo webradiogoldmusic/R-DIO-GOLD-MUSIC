1dce2ce1209442faa7811fc8049a1aef3ed66065
webradiogoldmusic <webradiogoldmusic@gmail.com>
2026-07-15 15:38:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -98,6 +98,70 @@ if(menuBtn && menu && overlay){
     });
 
 
+}
+}
+
+
+
+function fecharMenu(){
+
+    menu.classList.remove("active");
+
+    overlay.classList.remove("active");
+
+}
+
+
+
+if(menuBtn && menu && overlay){
+
+
+    menuBtn.addEventListener("click", () => {
+
+
+        if(menu.classList.contains("active")){
+
+            fecharMenu();
+
+        }else{
+
+            abrirMenu();
+
+        }
+
+
+    });
+
+
+
+    // Fecha clicando fora
+
+    overlay.addEventListener("click", () => {
+
+        fecharMenu();
+
+    });
+
+
+
+    // Fecha ao clicar em algum item
+
+    const links = document.querySelectorAll(".menu-links a");
+
+
+    links.forEach(link => {
+
+
+        link.addEventListener("click", () => {
+
+            fecharMenu();
+
+        });
+
+
+    });
+
+
 }            nav.style.display = "none";
         }
 
