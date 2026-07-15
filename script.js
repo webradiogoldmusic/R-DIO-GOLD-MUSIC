71c86431d1fab408472a356d150a4f3d3f953272
webradiogoldmusic <webradiogoldmusic@gmail.com>
2026-07-15 12:02:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// WebRádio Gold Music
 
+console.log("WebRádio Gold Music iniciada!");
+
+const ouvirBtn = document.querySelector(".banner button");
+
+ouvirBtn.addEventListener("click", () => {
+    alert("Em breve a transmissão da WebRádio Gold Music estará disponível!");
+});
