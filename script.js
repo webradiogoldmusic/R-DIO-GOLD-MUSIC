174631cfa38ec0618d174c6ecffc7551cd8b5103
webradiogoldmusic <webradiogoldmusic@gmail.com>
2026-07-15 15:16:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,41 @@
 
 console.log("WebRádio Gold Music iniciada!");
 
+
+// Botão ouvir rádio
 const ouvirBtn = document.querySelector(".banner button");
 
-ouvirBtn.addEventListener("click", () => {
-    alert("Em breve a transmissão da WebRádio Gold Music estará disponível!");
-});
+if (ouvirBtn) {
+    ouvirBtn.addEventListener("click", () => {
+        alert("Em breve a transmissão da WebRádio Gold Music estará disponível!");
+    });
+}
+
+
+// Menu Mobile ☰
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
+
+if (menuBtn && nav) {
+
+    menuBtn.addEventListener("click", () => {
+
+        if (nav.style.display === "flex") {
+            nav.style.display = "none";
+        } else {
+            nav.style.display = "flex";
+        }
+
+    });
+
+
+    // Fecha o menu clicando fora
+    document.addEventListener("click", (event) => {
+
+        if (!menuBtn.contains(event.target) && !nav.contains(event.target)) {
+            nav.style.display = "none";
+        }
+
+    });
+
+}
